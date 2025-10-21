af0f2f9b08018366ba0d330194ef39b8594a365f
Niakrist <ak_88dron@inbox.ru>
2025-10-21 11:36:18 +0300

    feat: add useDubounce

---
+++ src/hooks/useDebounse.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+interface IDebounceValue<T> {
+  debounceValue: T;
+}
+
+export const useDebounce = <T>(
+  value: T,
+  delay: number,
+): IDebounceValue<T> => {
+  const [debounceValue, setDebounceValue] = useState<T>(value);
+
+  console.log("value: ", value);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value, delay]);
+
+  return { debounceValue };
+};
