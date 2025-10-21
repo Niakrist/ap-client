9ced4a14b92fa10d771811a9b7d3ae18ea0eadd3
Niakrist <ak_88dron@inbox.ru>
2025-10-21 14:57:31 +0300

    feat: emplement UserProfile

---
+++ src/app/admin/users/(form)/fields/UserProfileFields.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Control,
+  Controller,
+  UseFormRegister,
+} from "react-hook-form";
+import { IUserFormState } from "../user-form.types";
+import styles from "./UserProfileFields.module.scss";
+import { Field } from "@/components/ui/Field/Field";
+import { UploadField } from "@/components/ui/UploadField/UploadField";
+
+export const UserProfileFields = ({
+  control,
+  register,
+}: {
+  control: Control<IUserFormState, any>;
+  register: UseFormRegister<IUserFormState>;
+}) => {
+  return (
+    <div className={styles.userProfile}>
+      <div>
+        <Field {...register("country")} placeholder="Country" />
+      </div>
+      <div>
+        <Controller
+          control={control}
+          name="avatarUrl"
+          render={({
+            field: { onChange, value },
+            fieldState: { error },
+          }) => {
+            return (
+              <UploadField
+                onChange={onChange}
+                value={value}
+                error={error}
+                folder="user"
+                placeholder="Avatar"
+              />
+            );
+          }}
+        />
+      </div>
+    </div>
+  );
+};
